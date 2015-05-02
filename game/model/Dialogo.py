a23faacc41028a6a41f5fc04e266d6c8bc4def5b
zangetsune <alex1991miralles@gmail.com>
2015-05-02 20:01:19 +0200

    Nuevo objeto Flecha

--- game/model/Dialogo.py
+++ game/model/Dialogo.py
@@ -3,9 +3,10 @@ from renpy import exports
 
 class Dialogo:
 
-    def __init__(self, who, what, whatFirst=None):
+    def __init__(self, who, what, whatFirst=None, sonido=None):
         self._who = who
         self._what = what
+        self._sonido = sonido
         if whatFirst is None:
             self._whatFirst = what
         else:
@@ -13,8 +14,12 @@ class Dialogo:
 
     def doTheThing(self):
         for linia in self._what:
+            if self._sonido is not None:
+                exports.play(self._sonido)
             exports.say(self._who, linia)
 
     def doTheFirstThing(self):
         for linia in self._whatFirst:
+            if self._sonido is not None:
+                exports.play(self._sonido)
             exports.say(self._who, linia)
---
+++ game/model/MostrarTexto.py
@@ -0,0 +1,10 @@
+__author__ = 'Isidre'
+
+from model import ItemEscena, Dialogo
+
+class MostrarTexto(ItemEscena):
+
+    def __init__(self, who, what, xpos, ypos, xmaximum, ymaximum, whatFirst=None, sonido=None, imgActivo=None, imgDesactivo=None):
+        ItemEscena.__init__(self, Dialogo(who, what, whatFirst, sonido))
+
+        self.setClickListener(xpos, ypos, xmaximum, ymaximum, imgActivo, imgDesactivo)
